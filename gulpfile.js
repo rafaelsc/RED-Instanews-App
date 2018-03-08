3891adf327bb661db9a8b4845b82f81e67b9d9c8
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-07 20:17:54 -0800

    Ads Browser-Sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,14 @@ var gulp = require("gulp"); // Load Gulp!
 var uglify = require("gulp-uglify"),
     rename = require("gulp-rename");
 
-gulp.task("default", function() {
-  return gulp
-    .src("./js/*.js") // What files do we want gulp to consume?
-    .pipe(uglify()) // Call the uglify function on these files
-    .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
-    .pipe(gulp.dest("./build/js")); // Where do we put the result?
+gulp.task("default", function () {
+    return gulp
+        .src("./js/*.js") // What files do we want gulp to consume?
+        .pipe(uglify()) // Call the uglify function on these files
+        .pipe(rename({extname: ".min.js"})) // Rename the uglified file
+        .pipe(gulp.dest("./build/js")); // Where do we put the result?
+});
+
+gulp.task('watch', function () {
+    gulp.watch('js/*.js', gulp.series('scripts'));
 });
