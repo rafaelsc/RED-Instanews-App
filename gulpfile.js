2fc716cfd2a006f840e94b168e30618023d997e1
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-08 20:35:28 -0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
-var gulp = require("gulp"); // Load Gulp!
-// Now that we've installed the uglify package we can require it:
-var uglify = require("gulp-uglify"),
-    rename = require("gulp-rename");
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
+const eslint = require("gulp-eslint");
 
-var browserSync = require('browser-sync').create();
+gulp.task("default", ["lint", "scripts", "browser-sync", "watch"]);
 
-gulp.task("default", function () {
-    return gulp
-        .src("./js/*.js") // What files do we want gulp to consume?
-        .pipe(uglify()) // Call the uglify function on these files
-        .pipe(rename({extname: ".min.js"})) // Rename the uglified file
-        .pipe(gulp.dest("./build/js")); // Where do we put the result?
+gulp.task("watch", () => {
+    gulp.watch(["javascripts/*.js", "css/*.css", "*.html"] , ["reload"]);
 });
 
-gulp.task('watch', function () {
-    gulp.watch('js/*.js', gulp.series('scripts'));
-});
-
-// Static server
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", () => {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 });
+
+gulp.task("reload", function() {
+    browserSync.reload();
+ });
+
+gulp.task("lint", () => {
+    return gulp.src(["./js/*.js","!node_modules/**"])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task("scripts", () => {
+    return gulp
+            .src("./js/*.js") // What files do we want gulp to consume?
+            .pipe(uglify()) // Call the uglify function on these files
+            .pipe(rename({extname: ".min.js"})) // Rename the uglified file
+            .pipe(gulp.dest("./build/js")); // Where do we put the result?
+  });
