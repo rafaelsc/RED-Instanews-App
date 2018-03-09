0a0098422d9658a1b6c57b23f118ca54d4f0f985
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-08 21:00:10 -0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(() => {
 
+    const $menu = $("#sections");
+    const $spin = $("#spin");
 
+    $spin.hide()
+
+    $menu.change(()=>{
+
+        console.log("menu change");
+
+        const val = $menu.val();
+        if(!val){
+            return;
+        }
+
+        console.log("Done");
+
+        $spin.show();
+    });
 
 });
