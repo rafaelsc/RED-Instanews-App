76a1825bf98e47e20b9a564d59a47c12892faf2c
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-10 14:44:15 -0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@ const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 const browserSync = require("browser-sync").create();
 const eslint = require("gulp-eslint");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
 
-gulp.task("default", ["lint", "scripts", "browser-sync", "watch"]);
+
+gulp.task("default", ["lint", "scripts", "sass", "browser-sync", "watch"]);
 
 gulp.task("watch", () => {
-    gulp.watch(["javascripts/*.js", "css/*.css", "*.html"] , ["reload"]);
+    gulp.watch(["js/*.js", "css/*.css", "css/*.scss", "*.html"], ["sass", "reload"]);
 });
 
 gulp.task("browser-sync", () => {
@@ -35,4 +39,16 @@ gulp.task("scripts", () => {
             .pipe(uglify()) // Call the uglify function on these files
             .pipe(rename({extname: ".min.js"})) // Rename the uglified file
             .pipe(gulp.dest("./build/js")); // Where do we put the result?
+  });
+
+
+gulp.task("sass", () => {
+    return gulp
+      .src("./css/main.scss")
+      .pipe(sass())
+      .pipe(autoprefixer({browsers: ["last 2 versions"]}))
+      .pipe(gulp.dest("./build/css"))
+      .pipe(cssnano())
+      .pipe(rename({extname: ".min.css"}))
+      .pipe(gulp.dest("./build/css"));
   });
